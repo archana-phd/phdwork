878baa88c000408f6b5ffaa8724960e1d865858f
Himanshu <shekhar.himanshu@gmail.com>
2022-01-02 01:40:03 +0530

    some more

---
+++ code/anamodel/pseudo.py
@@ -0,0 +1,32 @@
+'''
+Product data: Product code, category, entry date, total inventory, quality (rating) 
+Basic Prices: Base price, cost of storage/time, min and max sale price 
+Sales objective: Average Sale price, Time to sell, 
+Current Status: Remaining inventory, Current cost price
+Competition data: others price, other’s quality (if available)
+Market: Current demand, predicted demand in future based on history
+Explore factor: new product discount, new customer discount
+'''
+class ProductData: 
+    def __init__():
+        pass;
+
+class BasePrices: 
+    def __init__():
+        pass;
+
+
+class SalesObjective:
+    def __init__():
+        pass;
+
+class SalesStatus:
+    def __init__():
+        pass; 
+
+        
+class Product:
+    def __init__():
+        pass;
+
+#def calc_pric: 
