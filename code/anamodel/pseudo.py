c310228371c3f15523b2c6e3d6f4f8dcf7f02b99
Archana <shekhar.himanshu@gmail.com>
2022-01-03 01:22:21 +0530

    more

--- code/anamodel/pseudo.py
+++ code/anamodel/pseudo.py
@@ -8,28 +8,125 @@ Market: Current demand, predicted demand in future based on history
 Explore factor: new product discount, new customer discount
 '''
 class ProductData: 
-    def __init__():
-        pass;
+    @classmethod
+    def __init__(self, productCode, productCategory, launchDate, totalInventory, quality):
+        self.productCode = productCode;
+        self.productCategory = productCategory;
+        self.launchDate = launchDate;
+        self.totalInventory = totalInventory
+        self.quality = quality; #(1-5)
 
-class BasePrices: 
-    def __init__():
+    @staticmethod
+    def loadFromStore(productCode):
+        if(productCode == "iphone 13" || True):
+            return ProductData( productCode, "electronics-mobile", "2020-06-01", 1000, 5 );
+
+class PriceSettings: 
+    def __init__(self):
+        self.basePrice = 0;
+        self.minPrice = 0;
+        self.maxPrice = 0;
         pass;
 
 class SalesObjective:
-    def __init__():
+    def __init__(self):
         pass;
 
 class SalesStatus:
-    def __init__():
+    def __init__(self):
         pass; 
 
 class MarketTrend:
-    def __init__():
+    def __init__(self):
+        self.currentSellingRate = 0; ## 0 --> 1, more than 1 is high in demand
+        self.futureDemandAnticipation = 0;  ## 0 --> 1, more than 1 is high demand in future 
+
+class CompetitionData:
+    def __init__(self):
+        self.otherPrices = [
+            '''
+            {'quality': 'superior/same/worse', 'price': number}
+            '''
+        ]
+
         pass; 
+
+class DiscountFactor:
+    def __init__(self):
+        self.prudctFactor = 0;
+        self.customerFactor = 0;
 
 class Product:
     def __init__(self, productCode):
-        selpf.productData = ProductData(productCode); 
+        self.productData = ProductData.loadFromStore(productCode);
+        self.priceSettings = PriceSettings(productCode); 
+        self.discountFactor = DiscountFactor();
+        self.competitionData = CompetitionData();
+        self.marketTrend = MarketTrend();
+
+        self.calculatedPrice = 0;
         pass;
 
-#def calc_pric: 
+    def calcPrice(self):
+        if(self.MarketTrend.isOngoingHighDemand()):
+            self.calculatedPrice = self.priceSettings.maxPrice;
+
+        if(self.MarketTrend.isAnticipatedHighFutureDemand() > pendingInventoryRatio):
+            self.calculatedPrice = self.priceSettings.maxPrice;
+
+        # calculatr factors for discounting from the max price 
+        # rice points --> currentPrice --> maxPrice 
+        #
+        
+        demandFactor = 0;
+        customerFactor = 0; #find a number 
+        exploreFactor = 0;
+
+        priceFactor = calculatedPriceFactor( demandFactor, customerFactor, exploreFactor ); 
+
+        #please note price factor can be negative value, that is to clearup the inventories, to avoid storage cost and wastage due to perishabilty        
+
+        price = currentPrice + priceFactor*Range;
+        return price; 
+
+    def calculatedPriceFactor( demandFactor, customerFactor, exploreFactor, competitorData ):
+        #demand factor: this is should be most important factor
+        #explore factor should only be inverse of inventory remaining. 
+        #customer factor: customers can get discount, this is explicit 
+
+        priceFactor = 1
+
+        '''
+        We need to generate a table like this... this can be gernerated using numerical analysis, statistical analysis or ML techniques
+        ╔════════╤═════════╤══════════╤═══════╗
+        ║ Demand │ Explore │ Customer │ Price ║
+        ╠════════╪═════════╪══════════╪═══════╣
+        ║ 0.9    │ 1       │ 0.9      │ 0.9   ║
+        ╟────────┼─────────┼──────────┼───────╢
+        ║ 0.8    │ 0.9     │ 1        │ 0.75  ║
+        ╚════════╧═════════╧══════════╧═══════╝
+
+        We are trying to optimize the price offered which maximizes the possibility of purchase 
+        
+        priceFactor = tableLookpup( grid(demandFactor), grid(customerFactor), grid(exploreFactor))
+
+        Profit = ( Sum(Price-i*IS-i)) ) - (ItemProduce*Cost + FixedCost + StorageCost* + (ItemProduced-ItemSold)*PerItemCostOfDisposal )
+        
+        Conversion probability == Ideal 10%
+            - Average search rate. 
+            - Demand curve.
+                Price,CustomerType  --> #item sold
+                item to sell        --> Price 
+            - You need to have a daily target 
+                - TWAP
+                - Volume profile
+            - Reacting to ongoing demand
+                
+
+
+
+
+
+        '''
+
+        return priceFactor; 
